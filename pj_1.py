e92267081c0f649290762f92e7388b51c56318c8
jinho222 <jinho020@yonsei.ac.kr>
2023-10-17 13:07:24 +0900

    update....

--- pj_1.py
+++ pj_1.py
@@ -17,13 +17,13 @@ class NetworkSocket:
         # TCP server socket 생성
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((host, port))
+        sock.listen()
         return sock
 
     @staticmethod
     def tcp_server_connect(server_socket: socket.socket) -> Tuple[socket.socket, any]:
-        # Server_socket을 통해 client와의 connection 생성 
+        # Server_socket을 통해 client와의 connection 생성
         # 생성된 connection socket(conn)과 client의 address(tcp_client_addr) 반환
-        server_socket.listen()
         client_socket, addr = server_socket.accept()
         return (client_socket, addr)
 
@@ -44,38 +44,33 @@ class NetworkSocket:
 
     @staticmethod
     def udp_server_connect(udp_server_socket: socket.socket):
-        # udp_client_socket 함수가 전송한 packet으로부터 client의 udp address(udp_client_addr) 반환 
-        udp_server_socket.listen()
-        client_socket, addr = udp_server_socket.accept()
-        return (client_socket, addr)
+        # udp_client_socket 함수가 전송한 packet으로부터 client의 udp address(udp_client_addr) 반환
+        _, addr = udp_server_socket.recvfrom(1024)
+        return addr
 
     @staticmethod
     def udp_client_socket(host: str, port: int) -> socket.socket:
         # UDP client socket 생성
         # UDP 통신으로 server에 packet을 전송하고 udp client socket(udp_client_socket) 반환
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect((host, port))
+        sock.sendto("ack".encode(ENCODING), (host, port))
         return sock
 
     def tcp_send(self, data: bytes) -> None:
         # TCP socket(tcp_socket)을 통해 입력 받은 data 전송
-        print('send')
-        print(data)
-        self.tcp_socket.send(data)
+        self.tcp_socket.sendall(data)
 
     def udp_send(self, data: bytes) -> None:
-        # UDP socket(udp_socket)를 통해 상대방의 udp 주소(target_udp_addr)로 입력받은 data 전송 
-        self.udp_socket.send(data)
+        # UDP socket(udp_socket)를 통해 상대방의 udp 주소(target_udp_addr)로 입력받은 data 전송
+        self.udp_socket.sendto(data, self.target_udp_addr)
 
     def tcp_recv(self) -> bytes:
         # TCP socket(tcp_socket)으로 들어오는 packet의 data 반환
-        print('recv')
-        return self.tcp_socket.recv()
+        return self.tcp_socket.recv(1024)
 
     def udp_recv(self) -> bytes:
         # UDP socket(udp_socket)으로 들어오는 packet의 data 반환
-        return self.udp_socket.recv()
-
+        return self.udp_socket.recvfrom(1024)
 
     def close(self) -> None:
         try:
@@ -89,11 +84,17 @@ class NetworkSocket:
         except socket.error as msg:
             print(f"Unexpected {msg}, {type(msg)}")
 
-    def server_open_func(self, host: str = "", tcp_port: int = DEFAULT_TCP_PORT,
-                         udp_port: int = DEFAULT_UDP_PORT) -> int:
+    def server_open_func(
+        self,
+        host: str = "",
+        tcp_port: int = DEFAULT_TCP_PORT,
+        udp_port: int = DEFAULT_UDP_PORT,
+    ) -> int:
         try:
             server_socket = self.tcp_server_socket(host, tcp_port)
-            self.tcp_socket, self.target_tcp_addr = self.tcp_server_connect(server_socket)
+            self.tcp_socket, self.target_tcp_addr = self.tcp_server_connect(
+                server_socket
+            )
             self.udp_socket = self.udp_server_socket(host, udp_port)
             self.tcp_send("ack".encode(ENCODING))
             self.target_udp_addr = self.udp_server_connect(self.udp_socket)
@@ -103,8 +104,12 @@ class NetworkSocket:
             print(f"Unexpected {msg}, {type(msg)}")
             return -1
 
-    def client_connect_func(self, host: str = "", tcp_port: int = DEFAULT_TCP_PORT,
-                            udp_port: int = DEFAULT_UDP_PORT) -> int:
+    def client_connect_func(
+        self,
+        host: str = "",
+        tcp_port: int = DEFAULT_TCP_PORT,
+        udp_port: int = DEFAULT_UDP_PORT,
+    ) -> int:
         try:
             self.target_tcp_addr = (host, tcp_port)
             self.target_udp_addr = (host, udp_port)
