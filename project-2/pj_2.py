c04ff0f5c9208589608a5ff56f702bc7540ccf14
jinho222 <jinho020@yonsei.ac.kr>
2023-11-16 11:48:34 +0900

    declare udp_gbn function

--- project-2/pj_2.py
+++ project-2/pj_2.py
@@ -304,13 +304,16 @@ class FileTransfer:
         # def udp_gbn () or def udp_sr()로 구현
          # hint: self.udp_send_packet[ack_num]에 저장시
         # (send time, packet)형태로 저장할 것
-        ack_num = self.udp_ack_num
-        while ack_num != self.udp_last_ack_num:
-            if not self.udp_ack_windows[ack_num] and not self.udp_recv_flag[ack_num]:
-                packet = self.udp_send_packet[ack_num][1]
-                udp_send_func(packet)
-                self.udp_send_packet[ack_num] = (time(), packet)
-            ack_num = (ack_num + 1) % UDP_MAX_ACK_NUM
+        def udp_gbn():
+            ack_num = self.udp_ack_num
+            while ack_num != self.udp_last_ack_num:
+                if not self.udp_ack_windows[ack_num] and not self.udp_recv_flag[ack_num]:
+                    packet = self.udp_send_packet[ack_num][1]
+                    udp_send_func(packet)
+                    self.udp_send_packet[ack_num] = (time(), packet)
+                ack_num = (ack_num + 1) % UDP_MAX_ACK_NUM
+
+        udp_gbn()
         pass
 
     def udp_ack_send(self, ack_bytes: bytes, udp_send_func: Callable):
